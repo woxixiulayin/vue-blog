6f2d07ff595dc645f68af3704dd083747bb3c3b0
Jackson Liu <416883813@qq.com>
2016-08-15 23:07:05 +0800

    add header nav body footer vue file

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,34 @@
 <template>
-  <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
-  </div>
+  <app-header></app-header>
+  <app-nav></app-nav>
+  <app-body></app-body>
+  <app-footer></app-footer>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import appHeader from './components/Header.vue'
+import appNav from './components/Nav.vue'
+import appBody from './components/Body.vue'
+import appFooter from './components/Footer.vue'
 
 export default {
   components: {
-    Hello
+    appHeader,
+    appNav,
+    appBody,
+    appFooter
   }
 }
 </script>
 
 <style>
-html {
-  height: 100%;
-}
+/*重置样式*/
+body, html, div, blockquote, img, label, p, h1, h2, h3, h4, h5, h6, pre, ul, ol,
+li, dl, dt, dd, form, a, fieldset, input, th, td
+{margin: 0; padding: 0; border: 0; outline: none;}
+ul, ol{list-style: none;}
+a{color: black;text-decoration: none;}
+a:hover
+{text-decoration: underline;}
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>Ganker's空间</h1>
+  </header>
+</template>
+
+<script>
+  export default { }
+</script>
+
+<style lang='scss'>
+$bgcolor: rgb(0,127,195);
+$fontcolor: white;
+$fontsize: 3.5rem;
+$height: 8rem;
+
+header {
+  width: 100%;
+  height: $height;
+  background-color: $bgcolor;
+}
+
+h1 {
+  text-align: center;
+  line-height: $height;
+  font-size: $fontsize;
+  color: $fontcolor;
+}
+
+</style>
