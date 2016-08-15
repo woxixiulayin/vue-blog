1b16cefe97c0d58017f9e17aaf1191400b273f7c
Jackson Liu <416883813@qq.com>
2016-08-15 23:39:31 +0800

    nav style

--- src/App.vue
+++ src/App.vue
@@ -27,8 +27,8 @@ body, html, div, blockquote, img, label, p, h1, h2, h3, h4, h5, h6, pre, ul, ol,
 li, dl, dt, dd, form, a, fieldset, input, th, td
 {margin: 0; padding: 0; border: 0; outline: none;}
 ul, ol{list-style: none;}
-a{color: black;text-decoration: none;}
+a{color: white;text-decoration: none;}
 a:hover
-{text-decoration: underline;}
+{text-decoration: none;}
 
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="global-nav">
+    <ul class='menu'>
+      <li class='menu-item'><a href='#'>首页</a></li>
+      <li class='menu-item'><a href='#'>关于</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default { }
+</script>
+
+<style lang='scss'>
+$bgcolor: rgb(49,49,49);
+$fontcolor: white;
+$hovercolor: rgb(228,22,53);
+$fontsize: 1.2rem;
+$height: 2.5rem;
+$bodywidth: 50rem;
+
+.global-nav {
+  width: 100%;
+  background-color: $bgcolor;
+}
+
+.menu {
+  background: transparent;
+  width: $bodywidth;
+  margin: 0 auto;
+  overflow: hidden;
+}
+
+.menu-item {
+  float: left;
+  height: $height;
+  line-height: $height;
+  font-size: $fontsize;
+  color: $fontcolor;
+  padding:0 1.5rem;
+  transition: all 1s;
+  font-family: "Microsoft Yahei", Arial, Helvetica, sans-serif;
+}
+
+.menu-item:hover {
+  background-color: $hovercolor;
+}
+
+</style>
