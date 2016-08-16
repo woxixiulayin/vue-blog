e3ba27b71b590be3d74fe6d47978c59cc621d5dc
Jackson Liu <416883813@qq.com>
2016-08-16 15:31:04 +0800

    add vue-blog site into footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer id='footer'>
-    <div class="footer-info ">@{{ year }} <a href='#'>{{ author }}</a> Powered By <a :href='vueblogsite'>Vue-blog</a></div>
+    <div class="footer-info ">@{{ year }} <a href='#'>{{ author }}</a> Powered By <a :href='vueblogsite' target='_blank'>Vue-blog</a></div>
   </footer>
 </template>
 
@@ -11,26 +11,28 @@ export default {
   data: function () {
     return {
       year: new Date().getFullYear(),
-      author: config.author
+      author: config.author,
+      vueblogsite: 'https://github.com/woxixiulayin/vue-blog'
     }
   }
 }
 </script>
 
 <style lang='scss'>
-$fontcolor: blue;
+$linkcolor: rgb(37, 143, 184);
 $fontsize: 1rem;
 $height: 3.5rem;
-$bgcolor: rgb(217,237, 247);
+$bgcolor: rgb(38, 42, 48);
 
 #footer {
   text-align: center;
   background: $bgcolor;
+  color: #ccc;
 }
 
 #footer a {
   height: $height;
   line-height: $height;
-  color: $fontcolor;
+  color: $linkcolor;
 }
 </style>  
